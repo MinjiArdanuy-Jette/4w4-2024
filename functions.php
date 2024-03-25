e740419e4568bd94185b513d5100878b9d369979
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2024-03-25 00:45:45 -0400

    sRelâche Modification de l'effet de survol plus discret du menu

--- functions.php
+++ functions.php
@@ -10,21 +10,45 @@ function wp_custom_menus()
 }
 add_action('init', 'wp_custom_menus');
 
-function ajouter_html_a_la_fin_du_menu($items, $args)
+function ajouter_span_a_la_fin_de_la_balise_a($items, $args)
 {
     // Vérifie si c'est le bon menu principal
     if ($args->theme_location == 'principal') {
-        // Trouve la position de la dernière balise "</ul>"
-        $last_ul_pos = strrpos($items, '</ul>');
+        // Divise les éléments de menu en un tableau en utilisant "</li>" comme délimiteur
+        $menu_items = explode('</li>', $items);
 
-        // Ajoute la balise <span> après la dernière balise "</ul>" et avant la fin du <nav>
-        if ($last_ul_pos !== false) {
-            $items = substr($items, 0, $last_ul_pos) . '<span></span>' . substr($items, $last_ul_pos);
+        // Chaîne pour stocker les éléments de menu avec des spans
+        $menu_with_spans = '';
+
+        // Parcourir chaque élément de menu
+        foreach ($menu_items as $item) {
+            // Vérifier s'il y a un lien (<a>) dans l'élément de menu
+            if (strpos($item, '<a') !== false) {
+                // Divise l'élément de menu en deux parties : avant et après la balise "<a>"
+                $split_item = explode('<a', $item, 2);
+                // La première partie (avant la balise "<a>")
+                $part_before_a = $split_item[0];
+                // La deuxième partie (la balise "<a>" et le reste de l'élément de menu)
+                $part_with_a = '<a' . $split_item[1];
+                // Ajouter le span à la fin de l'intérieur de la balise "<a>"
+                $part_with_a = str_replace('</a>', '<span></span></a>', $part_with_a);
+                // Reconstruire l'élément de menu avec le span ajouté à la fin de la balise "<a>"
+                $item_with_span = $part_before_a . $part_with_a;
+                // Ajouter l'élément de menu modifié à la chaîne résultante
+                $menu_with_spans .= $item_with_span . '</li>';
+            } else {
+                // S'il n'y a pas de lien, ajouter simplement l'élément de menu sans span
+                $menu_with_spans .= $item . '</li>';
+            }
         }
+
+        // Remplacer les éléments de menu originaux par ceux avec des spans
+        $items = $menu_with_spans;
     }
+
     return $items;
 }
-add_filter('wp_nav_menu', 'ajouter_html_a_la_fin_du_menu', 10, 2);
+add_filter('wp_nav_menu', 'ajouter_span_a_la_fin_de_la_balise_a', 10, 2);
 add_theme_support('menus');
 add_theme_support('custom-logo');
 /*
