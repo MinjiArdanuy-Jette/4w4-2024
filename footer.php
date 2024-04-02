ed8e9a2d46bde51a96be6afd20872389d4044e88
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2024-04-02 12:34:55 -0400

    s8 Supprimer le h6 du footer

--- footer.php
+++ footer.php
@@ -7,18 +7,16 @@
 <div id="footer" class="global">
   <footer>
     <h2>Footer</h2>
-    <h6>
-      <?php
-      wp_nav_menu(
-        array(
-          'theme_location' => 'footer-menu',
-          'menu_class' => 'footer-menu',
-          'container' => 'nav',
-          'container_class' => 'footer-menu-container',
-        )
-      );
-      ?>
-    </h6>
+    <?php
+    wp_nav_menu(
+      array(
+        'theme_location' => 'footer-menu',
+        'menu_class' => 'footer-menu',
+        'container' => 'nav',
+        'container_class' => 'footer-menu-container',
+      )
+    );
+    ?>
   </footer>
 </div>
 </body>
