d93dd61fd918c0ecc03b5ba379fc5f265beb16a6
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2024-04-11 09:56:21 -0400

    s9c2 Ajout d'un formulaire de recherche sur la page Errwur 404

--- 404.php
+++ 404.php
@@ -1,13 +1,24 @@
 <?php get_header(); ?>
-<h1>404.php</h1>
 <!-- <//////////////// Contenu principal -->
 <div id="erreur_404" class="global">
   <section>
-    <h2>Erreur 404 </h2>
-    <p>Vous essayez d'accéder à une page qui n'existe pas</p>
-    <a href=" <?= get_bloginfo("url"); ?>">
-      <?= get_bloginfo("name"); ?>
-    </a>
+    <div class="erreur404-conteneur">
+      <h1>Erreur 404 </h1>
+      <p>Vous essayez d'accéder à une page qui n'existe pas</p>
+      <a href=" <?= get_bloginfo("url"); ?>">
+        <?= get_bloginfo("name"); ?>
+      </a>
+      <!-- Mettre le formulaire de recherche manuellement car de base il est trop différent du modèle de base -->
+      <form class="recherche_erreur" role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
+        <label>
+          <input class="recherche__input" type="search" class="search-field" placeholder="Search..."
+            value="<?php echo get_search_query(); ?>" name="s" />
+        </label>
+        <button class="recherche__bouton" type="submit" class="search-submit">
+          <span class="recherche__icone">&#x1F50D;</span>
+        </button>
+      </form>
+    </div>
   </section>
 </div>
 <!-- //////////////////// Pied de page -->
